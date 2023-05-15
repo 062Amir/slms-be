b540cb1de798eb18ff7d5c3c64428c37c232b076
Md. Amir Khan <amir.md@neosoftmail.com>
2023-05-15 19:53:14 +0530

    Minor update

--- index.ts
+++ index.ts
@@ -1,18 +1,19 @@
 import bodyParser from "body-parser";
 import * as dotenv from "dotenv";
 import express, { Express } from "express";
-import routes from "./routes/routes";
+import routes from "./src/routes/routes";
 import cors from "cors";
-import { dbConnect } from "./services/db.service";
+import { dbConnect } from "./src/services/db.service";
 
 const app: Express = express();
 dotenv.config();
 
-dbConnect();
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/", routes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server started at ${process.env.PORT} port`);
+dbConnect().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server started at ${process.env.PORT} port`);
+  });
 });
